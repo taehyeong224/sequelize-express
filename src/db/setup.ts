0fb1baed3ae9bf8b807128a0d05a5184a92bc389
liam <liam@ondelab.com>
2022-08-11 10:32:29 +0900

    test

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -1,6 +1,5 @@
 import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const sequelize = new Sequelize('test', 'root', '1234', {
-  host: 'localhost',
-  dialect: 'mysql',
-})
+export const sequelize = new Sequelize(process.env.MYSQL, { dialect: 'mysql' })
--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,41 @@ import { sequelize } from './db/setup'
 import { Sequelize } from 'sequelize'
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
+import glob from 'glob'
 
 dotenv.config()
+const app = express()
 
-const { PORT, ALTER_TABLE } = process.env
+const { PORT, ALTER_TABLE, MYSQL_HOST } = process.env
 
 async function bootstrap () {
   const port = PORT || 3000
-  const app = express()
 
   try {
     await sequelize.authenticate()
     console.log('Connection has been established successfully.')
     await sequelize.sync({ alter: ALTER_TABLE === 'true' })
   } catch (err) {
-    console.error('eeee : ', err)
     if (err.message.indexOf('Unknown database') >= 0) {
-      const sequelize = new Sequelize()
-      await sequelize.query(`CREATE DATABASE test;`)
+      const sq = new Sequelize(MYSQL_HOST, { dialect: 'mysql' })
+      await sq.query(`CREATE DATABASE test;`)
       await sequelize.sync({ alter: ALTER_TABLE === 'true' })
+      console.log('success ')
     }
   }
-
+  await loadRouter()
   app.listen(port, () => console.log('start'))
+}
+
+async function loadRouter () {
+  const routePath = __dirname + '/router/*.js'
+  console.log('routePath = ', routePath)
+  for (const file of glob.sync(routePath)) {
+    const { default: route } = await import(path.resolve(file))
+    console.log('route : ', route)
+    app.use(`/`, route())
+  }
 }
 
 (async () => {

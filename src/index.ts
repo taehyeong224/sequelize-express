4ee112af795dc17b819a48e59439b113e23d7bde
kimtaehyeong <taehyeong224@gmail.com>
2024-04-10 22:06:44 +0900

    edit bug

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import glob from "glob";
-import { errorHandler } from "./middleware/errorHandler";
-
+import dayjs from "dayjs";
+import { Request, Response } from "express";
+import { CommonError } from "./util/errorModel";
 dotenv.config();
 const app = express();
 
@@ -16,8 +17,8 @@ async function bootstrap() {
   await setupDB();
   setMiddleware();
   await loadRouter();
+  setErrorHandler();
   setErrorMiddleware();
-  app.use(errorHandler);
   app.listen(port, () => console.log("start"));
 }
 
@@ -30,6 +31,25 @@ function setErrorMiddleware() {
   app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500).send("server error");
+  });
+}
+
+function setErrorHandler() {
+  app.use(function (error: Error, req: Request, res: Response) {
+    console.error(
+      `[${dayjs().format("YYYY-MM-DD[T]HH:mm:ss+09:00")}] ${req.method} ${
+        req.originalUrl
+      } ${error.message}`
+    );
+
+    if (error instanceof CommonError) {
+      return res.status(error.statusCode).json({
+        code: error.errorCode,
+        statusCode: error.statusCode,
+        message: error.getErrorMessage(),
+      });
+    }
+    res.status(500).json({ msg: "server error" });
   });
 }
 
--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,24 +1,3 @@
-import { Request, Response } from "express";
-import dayjs from "dayjs";
-import { CommonError } from "../util/errorModel";
-
-export function errorHandler(error: Error, req: Request, res: Response) {
-  console.error(
-    `[${dayjs().format("YYYY-MM-DD[T]HH:mm:ss+09:00")}] ${req.method} ${
-      req.originalUrl
-    } ${error.message}`
-  );
-
-  if (error instanceof CommonError) {
-    return res.status(error.statusCode).json({
-      code: error.errorCode,
-      statusCode: error.statusCode,
-      message: error.getErrorMessage(),
-    });
-  }
-  res.status(500).json({ msg: "server error" });
-}
-
 export const asyncMiddleware = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
